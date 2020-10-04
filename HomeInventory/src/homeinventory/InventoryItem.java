8097189770047c3580fb7e79b6ab62e841bca243
RittikPradhan <37213303+RittikPradhan@users.noreply.github.com>
2020-10-05 02:40:19 +0530

    Add files via upload

---
+++ HomeInventory/src/homeinventory/InventoryItem.java
@@ -0,0 +1,23 @@
+/**
+Project 2 :- Home Inventory
+*/
+package homeinventory;
+/**
+ *
+ * @author Rittik Pradhan
+ */
+public class InventoryItem {
+public String description;
+public String location;
+public boolean marked;
+public String serialNumber;
+public String purchasePrice;
+public String purchaseDate;
+public String purchaseLocation;
+public String note;
+public String photoFile;
+    
+}
+
+
+/** ~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~ Successfully Implemented  ~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~ */
